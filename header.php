72fe2bdbad331ad0c0e6687b10cd6b87b0015fbc
Serge Jerkezian <serge.jerkezian@gmail.com>
2015-08-08 20:52:13 +0300

    main files

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Laura</title>
+
+    <script src="./scripts/jquery-1.11.3.min.js"></script>
+    <script src="./scripts/bootstrap.min.js"></script>
+    
+    <link rel="stylesheet" href="./style/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./style/css/style.css" />
+
+</head>
+
+<body>
